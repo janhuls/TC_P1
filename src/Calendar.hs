47b4d2865c81fa33998964012fb0045bc02d2c72
Jan Huls <j.huls@students.uu.nl>
2025-11-22 13:57:39 +0100

    opgave 6 en 7

--- src/Calendar.hs
+++ src/Calendar.hs
@@ -7,10 +7,30 @@ import ParseLib.Derived
 
 -- Exercise 6
 data Calendar = Calendar
+  { prodid :: Prodid,
+    version :: Version,
+    events :: [Event]
+  }
   deriving (Eq, Ord, Show)
 
+newtype Prodid = Prodid String deriving (Eq, Ord, Show, Generic, CustomData)
+newtype Version = Version String deriving (Eq, Ord, Show, Generic, CustomData)
+
 data Event = Event
+  { dtStamp :: DateTime,
+    uid :: UID,
+    dtStart :: DateTime,
+    dtEnd :: DateTime,
+    description :: Description,
+    summary :: Summary,
+    location :: Location
+  }
   deriving (Eq, Ord, Show)
+
+newtype UID = UID String deriving (Eq, Ord, Show, Generic, CustomData)
+newtype Description = Descr String deriving (Eq, Ord, Show, Generic, CustomData)
+newtype Summary = Summ String deriving (Eq, Ord, Show, Generic, CustomData)
+newtype Location = Loc String deriving (Eq, Ord, Show, Generic, CustomData)
 
 -- If you plan on using your own types in Calendar, Event, or Token. Make sure it derives Eq, Generic, and CustomData.
 -- Example:
@@ -18,11 +38,29 @@ data Event = Event
 --   deriving (Eq, Ord, Show, Generic, CustomData)
 
 -- Exercise 7
-data Token = Token
+data Token = Token String String --key value
   deriving (Eq, Ord, Show)
 
 lexCalendar :: Parser Char [Token]
-lexCalendar = undefined
+lexCalendar = many lexLine
+
+lexLine :: Parser Char Token
+lexLine = Token <$> keyParser <* symbol ':' <*> valueParser <* crlfParser
+
+crlfParser :: Parser Char String
+crlfParser = token "\r\n"
+
+keyParser :: Parser Char String
+keyParser = many1 (satisfy (/= ':'))
+
+valueParser :: Parser Char String
+valueParser = (++) <$> firstLine <*> restLines where
+  firstLine = many (satisfy (/= '\r'))
+  restLines = concat <$> many
+            (   crlfParser
+             *> symbol ' '
+             *> ((++) "\n" <$> many (satisfy (/= '\r')))
+            )
 
 parseCalendar :: Parser Token Calendar
 parseCalendar = undefined
